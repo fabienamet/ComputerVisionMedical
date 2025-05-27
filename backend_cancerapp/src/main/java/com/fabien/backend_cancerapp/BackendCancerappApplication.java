f4c409ce55b4a13eabf58c63ab38c350d383ea22
fabien amet <famet@thinkdeep.ai>
2025-05-27 05:12:37 +0200

    init SpringBoot backend

---
+++ backend_cancerapp/src/main/java/com/fabien/backend_cancerapp/BackendCancerappApplication.java
@@ -0,0 +1,13 @@
+package com.fabien.backend_cancerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCancerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCancerappApplication.class, args);
+	}
+
+}
---
+++ backend_cancerapp/src/test/java/com/fabien/backend_cancerapp/BackendCancerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabien.backend_cancerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCancerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
